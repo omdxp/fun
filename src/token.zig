7176315035ad8400bb22bd26395103d2fe147340
omdxp <omarbelghaouti@gmail.com>
2025-01-04 15:23:36 +0100

    feat: add TokenData union for enhanced token representation

--- src/token.zig
+++ src/token.zig
@@ -1,1 +1,18 @@
-pub const TokenType = enum { Identifier, Keyword, Operator, Symbol, Number, String, Comment, NewLine };
+pub const TokenType = enum {
+    Identifier,
+    Keyword,
+    Operator,
+    Symbol,
+    Number,
+    String,
+    Comment,
+    NewLine,
+};
+
+pub const TokenData = union {
+    cval: u8,
+    sval: []u8,
+    inum: c_int,
+    lnum: c_long,
+    llnum: c_longlong,
+};
